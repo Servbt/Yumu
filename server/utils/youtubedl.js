6d097ca9114f46cb58ce3c2209c411c83fd629e3
AREZVANI95 <105834594+Servbt@users.noreply.github.com>
2022-08-31 13:54:42 -0400

    sample data and proper npm packages aquired

---
+++ server/utils/youtubedl.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+// const ytdl = require('ytdl-core');
+// const ytpl = require('ytpl');
+
+// const  playlist =  ytpl('https://www.youtube.com/playlist?list=PLGZ11jE8mBFJsw2dOyR1lwXrq7f8LSJ1s');
+
+// console.log( playlist);
+ytdl('https://www.youtube.com/watch?v=Rdx_b7YrWsE', {quality: 'highest'})
+  .pipe(fs.createWriteStream('video.mp4'));
+
+
