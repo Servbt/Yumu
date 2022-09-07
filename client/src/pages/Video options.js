c0abe5c092a3fbe215a68ec3cdcf1923fa28fc3d
AREZVANI95 <105834594+Servbt@users.noreply.github.com>
2022-09-07 05:49:55 -0400

    back to previous commit, I just need to figure out how to assign an id to a selected video for storage

--- client/src/pages/Video options.js
+++ client/src/pages/Video options.js
@@ -1,69 +1,33 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import { useQuery } from '@apollo/client';
-import { QUERY_VIDEOS } from '../utils/queries';
+
 import Playlist from '../components/Playlist';
 import { useStoreContext } from '../utils/GlobalState';
 import {
   REMOVE_FROM_PLAYLIST,
   ADD_TO_PLAYLIST,
-  ADD_VIDEO,
 } from '../utils/actions';
 import { idbPromise } from '../utils/helpers';
 
-
 const VideoOptions = ( currentVideo ) => {
-  // console.log(currentVideo);
+  console.log(currentVideo);
   const [state, dispatch] = useStoreContext();
   const { id } = useParams();
 
-  const [ , setCurrentVideo] = useState({})
-  const { loading, data } = useQuery(QUERY_VIDEOS);
-  const { videos, playlist } = state;
+  // const [currentVideo , setCurrentVideo   ] = useState({})
 
-  useEffect(() => {
-    // already in global store
-    if (videos) {
-      setCurrentVideo(videos.find((video) => video._id === id));
-    } else if (!videos.find((video)=> video.id)){
-      dispatch({
-        type: ADD_VIDEO,
-        videos: currentVideo,
-      });
-      idbPromise('video', 'put', currentVideo);
-    }
-    // retrieved from server
-    // else if (data) {
-    //   dispatch({
-    //     type: QUERY_VIDEOS,
-    //     videos: data.video,
-    //   });
 
-    //   data.videos.forEach((video) => {
-    //     idbPromise('video', 'put', video);
-    //   });
-    // }
-    // get cache from idb
-    else if (!loading) {
-      idbPromise('video', 'get').then((indexedVideos) => {
-        dispatch({
-          type: QUERY_VIDEOS,
-          videos: indexedVideos,
-        });
-      });
-    }
-  }, [videos, data, loading, dispatch, id]);
+  const { playlist } = state;
 
 
   const addtoPlaylist = () => {
     // const videoInPlaylist = playlist.find((playlistItem) => playlistItem._id === id);
     // if (videoInPlaylist) {
-      setCurrentVideo(currentVideo._id === id);
       dispatch({
         type: ADD_TO_PLAYLIST,
         video: { currentVideo, },
       });
-      idbPromise('playlist', 'put', { currentVideo});
+      idbPromise('playlist', 'put', { currentVideo, id});
     }
   // };
 
