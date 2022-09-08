62b979f21452d78c44e7959fe2a9bcfc4a3bb53f
AREZVANI95 <105834594+Servbt@users.noreply.github.com>
2022-09-08 19:22:27 -0400

    forgot to add mongoose.connect...

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,9 +1,11 @@
 const mongoose = require('mongoose');
 
-process.env.MONGODB_URI || 'mongodb://localhost:27017/mern-shopping',
-{
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-}
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost:27017/mern-shopping',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+);
 
 module.exports = mongoose.connection;
