3d54a27f04be508d240c082d9c7f6c3f0408ab19
AREZVANI95 <105834594+Servbt@users.noreply.github.com>
2022-09-08 20:58:57 -0400

    compiled successfully

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(
+mongoose.connect( 
   process.env.MONGODB_URI || 'mongodb://localhost:27017/mern-shopping',
   {
     useNewUrlParser: true,
